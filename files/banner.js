972d246c6be492f4f8cb11d2d2507b068d734fba
Bhajneet S.K <bhajneet@gmail.com>
2025-05-31 15:52:52 -0400

    add minimal mode, hide empty banner items, and add year to datetime

--- files/banner.js
+++ files/banner.js
@@ -1,7 +1,13 @@
-var customBannerItems = [
-  "2025 _____ Annual Akhand Keertan Smaagam at _____ Gurdwara",
-  "WhatsApp Contact at +5 5555555555",
-];
+// Fill in custom banner items here between double quotes
+// The usual banner items are automatically added
+// E.g. "Visit www.AKJ.org for recordings, literature, and more"
+
+var customBannerItems = ["", "", ""];
+
+// Do not touch anything below this line unless you know what you're doing
+
+var isBanner =
+  params.has("minimal") && params.get("minimal") === "true" ? false : true;
 
 var bannerItems = params.has("banner")
   ? params.getAll("banner")
